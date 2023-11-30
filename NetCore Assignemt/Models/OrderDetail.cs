edd3dcf5dc5a735701a420ffa8ec1f5593e5a262
Cuongdzwl <xxxcuongxxx18@gmail.com>
2023-11-30 12:04:34 +0700

    Add Some Controller, Fix Some Bugs

---
+++ NetCore Assignemt/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore_Assignemt.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Key]
+        public int OrderId { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
