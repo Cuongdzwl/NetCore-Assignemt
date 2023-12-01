5f893287e3c53973085c6125f3e194e2a664f67d
Cuongdzwl <xxxcuongxxx18@gmail.com>
2023-12-01 14:41:22 +0700

    Edit Order

---
+++ NetCore Assignemt/Services/IOrderServices.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_Assignemt.Models;
+
+namespace NetCore_Assignemt.Services
+{
+    public interface IOrderServices
+    {
+        public Task<IActionResult> Pay(Order order);
+
+    }
+}
